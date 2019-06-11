94851d8794e219eba59f07c4d638a3e7e639bcfe
Johnny Morganz <19635171+JohnnyMorganz@users.noreply.github.com>
2019-06-11 19:16:22 +0100

    package

---
+++ package.lua
@@ -0,0 +1,9 @@
+return {
+  name = 'JohnnyMorganz/discordia-lavalink',
+  version = '0.0.1',
+  homepage = 'https://github.com/JohnnyMorganz/discordia-lavalink',
+  license = 'MIT',
+  files = {
+    '**.lua'
+  }
+}
